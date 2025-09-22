804df4766f588db049e6d3778fcc918df555f357
Ecl4ts <233498003+Ecl4ts@users.noreply.github.com>
2025-09-22 21:36:04 +0100

    Delete FixButton.lua

--- FixButton.lua
+++
@@ -1,6 +0,0 @@
--- script should be in StartGUI > Textbutton > LocalScript
-
-script.Parent.MouseButton1Down:Connect(function()
-    print('Attempting to restart API requests.')
-    game.ReplicatedStorage.RestartAPIRequests:FireServer()
-end)
